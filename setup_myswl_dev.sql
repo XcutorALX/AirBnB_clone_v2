6a8e725c1438e549144f1c01e09d4a69086104d6
Xcutor <Lekanabass50@gmail.com>
2024-04-20 18:44:24 -0500

    task done

---
+++ setup_myswl_dev.sql
@@ -0,0 +1,15 @@
+-- This script sets up the MySQL database for the development environment.
+-- Create the database 
+CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+-- Uses the database
+USE `hbnb_dev_db`;
+-- Create the user
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant all privileges to the user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Flush the privileges
+FLUSH PRIVILEGES;
+-- Grant select on performance schema
+GRANT SELECT ON performance_Schema.* TO 'hbnb_dev'@'localhost';
+-- Flush the privileges
+FLUSH PRIVILEGES;
