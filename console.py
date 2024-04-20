ae39ad8f0d97af68154834371e7276938033a3c4
Xcutor <Lekanabass50@gmail.com>
2024-04-20 17:29:52 -0500

    task done

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """ Console Module """
 import cmd
 import sys
+import re
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
@@ -73,7 +74,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -115,16 +116,29 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-        if not args:
+        restricted = ('id', 'created_at', 'updated_at', '__class__')
+        arg = self.parser(args)
+        cls_name = arg[0]
+        obj_arg = dict()
+        for val in arg[1:]:
+            val = val.split("=")
+            if len(val) >= 2:
+                obj_arg[val[0]] = self.parse_value(val[1])
+            else:
+                obj_arg[val[0]] = None
+
+        if not cls_name:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
+        elif cls_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-        new_instance = HBNBCommand.classes[args]()
+        new_instance = HBNBCommand.classes[cls_name]()
+        for key, val in obj_arg.items():
+            if key not in restricted:
+                setattr(new_instance, key, val)
         storage.save()
         print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -187,7 +201,7 @@ class HBNBCommand(cmd.Cmd):
         key = c_name + "." + c_id
 
         try:
-            del(storage.all()[key])
+            del (storage.all()[key])
             storage.save()
         except KeyError:
             print("** no instance found **")
@@ -272,7 +286,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +294,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
@@ -319,6 +333,48 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
+    @staticmethod
+    def parser(arg):
+        args = list()
+        split = 1
+        start = 0
+        stop = 0
+
+        for i in arg:
+            if i == " " and split == 1:
+                args.append(arg[start:stop].lstrip(" "))
+                start = stop
+            elif i == "\"":
+                split ^= 1
+            stop += 1
+
+        if start != stop:
+            args.append(arg[start:stop].lstrip(" "))
+
+        if not args:
+            return [None]
+        return args
+
+    @staticmethod
+    def parse_value(value):
+        str_pattern = r'^"([^"]*)"'
+        float_pattern = r'^(\d+\.\d+)'
+        int_pattern = r'^(\d+)'
+
+        str_match = re.match(str_pattern, value)
+        if str_match:
+            return str_match.group(1).replace('_', ' ')
+        else:
+            float_match = re.match(float_pattern, value)
+            if float_match:
+                return float(float_match.group(1))
+            else:
+                try:
+                    return (int(value))
+                except ValueError:
+                    return (None)
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
