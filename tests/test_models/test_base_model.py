8ef89e619927f51a2fe45f08e150a7bac68b8b70
Xcutor <Lekanabass50@gmail.com>
2024-04-20 16:12:06 -0500

    task done

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -24,7 +24,7 @@ class test_basemodel(unittest.TestCase):
     def tearDown(self):
         try:
             os.remove('file.json')
-        except:
+        except Exception as e:
             pass
 
     def test_default(self):
@@ -97,3 +97,87 @@ class test_basemodel(unittest.TestCase):
         n = new.to_dict()
         new = BaseModel(**n)
         self.assertFalse(new.created_at == new.updated_at)
+
+
+class TestCodeFormat(unittest.TestCase):
+    """A class to test pep8 on base_model file"""
+
+    def test_pycodestyle(self):
+        """Test pep8 format"""
+        pycodestyle_checker = pycodestyle.StyleGuide(quiet=True)
+        result = pycodestyle_checker.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+
+class TestDocstrings(unittest.TestCase):
+    """Test docstrings"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the test"""
+        cls.obj_members = inspect.getmembers(BaseModel, inspect.isfunction)
+
+    def test_docstrings(self):
+        """Check for docstrings"""
+        for name, func in self.obj_members:
+            self.assertIsNotNone(func.__doc__)
+
+
+class TestBaseModel(unittest.TestCase):
+    """Test cases for the BaseModel class"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up for the test"""
+        cls.base = BaseModel()
+        cls.base.name = "Kev"
+        cls.base.num = 20
+
+    @classmethod
+    def tearDownClass(cls):
+        """Clean up after all tests have been completed"""
+        del cls.base
+
+    def tearDown(self):
+        """Clean up after each test"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_pep8_BaseModel(self):
+        """Test for PEP8 compliance"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(['models/base_model.py'])
+        self.assertEqual(p.total_errors, 0, "Fix PEP8")
+
+    def test_checking_for_docstring_BaseModel(self):
+        """Check for docstrings"""
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.__init__.__doc__)
+        self.assertIsNotNone(BaseModel.__str__.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+
+    def test_method_BaseModel(self):
+        """Check if BaseModel has methods"""
+        self.assertTrue(hasattr(BaseModel, "__init__"))
+        self.assertTrue(hasattr(BaseModel, "save"))
+        self.assertTrue(hasattr(BaseModel, "to_dict"))
+
+    def test_init_BaseModel(self):
+        """Test if the base is an instance of BaseModel"""
+        self.assertIsInstance(self.base, BaseModel)
+
+    def test_save_BaseModel(self):
+        """Test if the save method works"""
+        self.base.save()
+        self.assertNotEqual(self.base.created_at, self.base.updated_at)
+
+    def test_to_dict_BaseModel(self):
+        """Test if dictionary conversion works"""
+        base_dict = self.base.to_dict()
+        self.assertEqual(self.base.__class__.__name__, 'BaseModel')
+        self.assertIsInstance(base_dict['created_at'], str)
+        self.assertIsInstance(base_dict['updated_at'], str)
