df6c6b27387556c2a1996b53d97602e8bd0f21b6
Xcutor <Lekanabass50@gmail.com>
2024-04-20 16:25:10 -0500

    task done

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ """
+
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 
 
-class test_review(test_basemodel):
+class TestReview(test_basemodel):
     """ """
 
     def __init__(self, *args, **kwargs):
