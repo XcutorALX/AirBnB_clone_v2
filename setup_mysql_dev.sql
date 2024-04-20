814bc0222237fc4c8b8aa01e9acd2e069567d6c7
Xcutor <Lekanabass50@gmail.com>
2024-04-20 18:57:02 -0500

    task done

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,5 +7,9 @@ USE `hbnb_dev_db`;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 -- Grants all privileges on the database hbnd_dev_db to the user hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Flushes the privileges
+FLUSH PRIVILEGES;
 -- Grants SELECT on the database performance_Schema to the user hbnb_dev
 GRANT SELECT ON performance_Schema.* TO 'hbnb_dev'@'localhost';
+-- Flushes the privileges
+FLUSH PRIVILEGES;
