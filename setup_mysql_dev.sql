93971bbc6289f97c4b307bacb4a880f9afec5ec3
Xcutor <Lekanabass50@gmail.com>
2024-04-20 18:55:53 -0500

    task done

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,11 @@
 -- This script sets up the MySQL database for the development environment.
--- It creates the database, the user, and the tables.
+-- Creates a database named hbnb_dev_Db
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
+-- Uses the database
 USE `hbnb_dev_db`;
+-- Creates a table named `hbnb_dev`
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grants all privileges on the database hbnd_dev_db to the user hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- Grants SELECT on the database performance_Schema to the user hbnb_dev
 GRANT SELECT ON performance_Schema.* TO 'hbnb_dev'@'localhost';
