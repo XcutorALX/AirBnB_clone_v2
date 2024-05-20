427a309bbea78465b6c7f26066550231cfe562f2
Xcutor <Lekanabass50@gmail.com>
2024-05-19 19:28:51 -0500

    task done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-# This script starts a Flask web application
+"""
+    This script starts a Flask web application
+"""
 from flask import Flask
 
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
-    """Root root for flask app"""
+    """
+        Root root for flask app
+    """
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(host="0.0.0.0")
