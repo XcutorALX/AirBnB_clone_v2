95aae02f50089b713153f92b176f132e77289937
Xcutor <Lekanabass50@gmail.com>
2024-05-22 22:52:40 -0500

    task done

--- models/city.py
+++ models/city.py
@@ -12,7 +12,7 @@ class City(BaseModel, Base):
     name = Column(String(128), nullable=False)
     state_id = Column(
         String(60),
-        ForeignKey('states.id'),
+        ForeignKey('states.id', ondelete='CASCADE', onupdate='CASCADE'),
         nullable=False
         )
 
---
+++ web_flask/9-states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    This script starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def states_list():
+    """
+        states_list route for flask app
+    """
+
+    return render_template('8-cities_by_states.html',
+                           states=storage.all("State"))
+
+
+@app.teardown_appcontext
+def teardown(err):
+    """
+        Teardown function
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
